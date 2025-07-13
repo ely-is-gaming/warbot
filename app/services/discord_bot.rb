8bc3ceb506ff4a776a7a2aff58870273eaa9b979
ely-is-gaming <wintermoonglade@outlook.com>
2025-07-13 12:20:28 -0400

    feat: add reaction response for when moderators approve/deny drop

--- app/services/discord_bot.rb
+++ app/services/discord_bot.rb
@@ -47,7 +47,7 @@ class DiscordBot
       # text = "Received #{event.user.username}, channel name: #{team}, drop: #{event.options["drop_name"]}, img_url: #{image_url}"
       # text = "**#{username}** received **#{drop_name}**!!"
 
-    embed_title = "#{drop_name} received from ##{team}!"
+    embed_title = "#{drop_name} received from ##{team}"
     embed_description = "Submitted by **#{username}**"
     embed_color = 0x00bfff
     embed_timestamp = Time.now
@@ -56,7 +56,7 @@ class DiscordBot
     event.respond(
       embeds: [
         {
-          title: embed_title,
+          title: "#{embed_title}!",
           description: embed_description,
           image: { url: image_url },
           color: embed_color,
@@ -67,13 +67,47 @@ class DiscordBot
 
     # Also post to review channel
     review_channel = event.bot.channel(WAR_REVIEW_CHANNEL_ID)
-    review_channel.send_embed do |embed|
-      embed.title = "#{embed_title} React with ✅ to approve or ❌ to deny."
+    msg = review_channel.send_embed do |embed|
+      embed.title = "#{embed_title}! React with ✅ to approve or ❌ to deny."
       embed.description = embed_description
       embed.image = Discordrb::Webhooks::EmbedImage.new(url: image_url)
       embed.color = embed_color
       embed.timestamp = embed_timestamp
     end
+
+    # make it easier for admins to reply
+    msg.create_reaction("✅")
+    msg.create_reaction("❌")
+
+    # take action on reviews or denials
+    handler = bot.add_await!(Discordrb::Events::ReactionAddEvent) do |reaction_event|
+    # Only handle reactions to the review message
+    next unless reaction_event.message.id == msg.id
+    next unless reaction_event.channel.id == WAR_REVIEW_CHANNEL_ID
+
+    # Role check
+    member = reaction_event.server.member(reaction_event.user.id)
+    deputy_role = reaction_event.server.roles.find { |r| r.name == "Deputy Owners" }
+
+    next unless member.role?(deputy_role)
+
+    # I can't believe I'm making a switch case for an emoji
+    emoji = reaction_event.emoji.name
+
+    case emoji
+    when "✅"
+      review_channel.send_message("✅ #{embed_title} approved by #{reaction_event.user.display_name}!")
+      # maybe update DB or add a role or whatever
+    when "❌"
+      review_channel.send_message("❌ #{embed_title} denied by #{reaction_event.user.display_name}.")
+      # trigger rejection logic here
+    else 
+      next # ignore subsequent reactions after the initial one
+    end
+
+    true # resolve the await so we are not awaiting more actions
+  end
+
   end
 
 
