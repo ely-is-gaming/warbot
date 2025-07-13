7c9efba526abaf19459408bd485f7fc1c1b63be8
ely-is-gaming <wintermoonglade@outlook.com>
2025-07-13 01:00:51 -0400

    feat: now send embedded message with image to review channel

--- app/services/discord_bot.rb
+++ app/services/discord_bot.rb
@@ -6,6 +6,7 @@ class DiscordBot
   BOT_TOKEN = ENV['DISCORD_BOT_TOKEN']
   CLIENT_ID = ENV['DISCORD_CLIENT_ID']
   GUILD_ID = ENV['DISCORD_GUILD_ID'].to_i # for guild command registration (dev/testing)
+  WAR_REVIEW_CHANNEL_ID = 1393812141311660052
 
   def self.run
     bot = Discordrb::Commands::CommandBot.new(
@@ -37,16 +38,43 @@ class DiscordBot
       drop_photo_id = event.options["drop_photo"]
       # [46] pry(DiscordBot)> event.resolved.attachments[1393701689034281071].proxy_url
       image_url = event.resolved.attachments[drop_photo_id.to_i].proxy_url
-      channel_name = event.channel.name
-      text = "Received #{event.user.username}, channel name: #{channel_name}, drop: #{event.options["drop_name"]}, img_url: #{image_url}"
-      event.respond(content: text)
+      team = event.channel.name
+      drop_name = event.options["drop_name"]
+      # username = event.user.username
+      username = event.server.member(event.user.id).display_name
 
-      # event.channel.send_embed do |embed|
-      #   embed.title = "test"
-      #   embed.image = Discordrb::Webhooks::EmbedImage.new(url: 'https://example.com/image.png')
-      #   embed.color = 0x00bfff
-      # end
+
+      # text = "Received #{event.user.username}, channel name: #{team}, drop: #{event.options["drop_name"]}, img_url: #{image_url}"
+      # text = "**#{username}** received **#{drop_name}**!!"
+
+    embed_title = "#{drop_name} received from #{team}!"
+    embed_description = "Submitted by **#{username}**"
+    embed_color = 0x00bfff
+    embed_timestamp = Time.now
+
+    # Respond in current channel
+    event.respond(
+      embeds: [
+        {
+          title: embed_title,
+          description: embed_description,
+          image: { url: image_url },
+          color: embed_color,
+          timestamp: embed_timestamp.iso8601
+        }
+      ]
+    )
+
+    # Also post to review channel
+    review_channel = event.bot.channel(WAR_REVIEW_CHANNEL_ID)
+    review_channel.send_embed do |embed|
+      embed.title = embed_title
+      embed.description = embed_description
+      embed.image = Discordrb::Webhooks::EmbedImage.new(url: image_url)
+      embed.color = embed_color
+      embed.timestamp = embed_timestamp
     end
+  end
 
 
     # bot.application_command(:hello, 'Say hello') do |event|
