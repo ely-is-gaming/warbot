bced6194f7614a56af77223efb6feb4f2f45cb74
ely-is-gaming <wintermoonglade@outlook.com>
2025-07-27 22:16:06 -0400

    feat: admins can now update drop name

--- app/commands/update_drop.rb
+++ app/commands/update_drop.rb
@@ -4,6 +4,7 @@ module Commands
     def self.register(bot)
         bot.register_application_command(:update_drop, 'Update a specific drop by drop ID') do |cmd|
             cmd.integer(:id, 'ID of drop to update', required: true)
+            cmd.string(:drop_name, 'update drop name', required: false)
             cmd.string(:team, 'update team', required: false)
             cmd.string(:owner, 'update owner', required: false)
             cmd.string(:submitter, 'update submitter', required: false)
@@ -11,51 +12,55 @@ module Commands
             cmd.string(:status, 'update status (approved or denied)', required: false)
 
             bot.application_command(:update_drop) do |event|
-
-                params = {
-                    id: event.options["id"],
-                    team: event.options["team"].present? ? Team.find_or_create_by(name: event.options["team"].present?) : nil,
-                    owner: event.options["owner"],
-                    submitter: event.options["submitter"],
-                    reviewed_by: event.options["reviewed_by"],
-                    status: event.options["status"].downcase
-                }
-
-                # remove nil values!
-                params.compact!
-
-                # Get the member who called the command
-                member = event.user.on(event.server)
-
-                # Find the role named "Deputy Owners"
-                deputy_role = event.server.roles.find { |r| r.name == "Deputy Owners" }
-
-                unless deputy_role.present? && member&.role?(deputy_role)
-                    Rails.logger.info("User #{event.server.member(event.user.id).display_name} tried to update drops but lacked the correct role.")
-                    event.respond(content: "Sorry, you don't have permission to use this command.", ephemeral: true)
-                    next
-                end
-
-                err = validate(params)
-                if err.present?
-                    Rails.logger.info("User #{event.server.member(event.user.id).display_name} tried to update drops but an err occurred: #{err}.")
-                    event.respond(content: err, ephemeral: true)
-                    next
-                end
-
-                begin
-                Drop.transaction do
-                    drop = Drop.find_by_id(params[:id])
-                    if drop.update!(params)
-                        Rails.logger.info("User #{event.server.member(event.user.id).display_name} updated drop ID #{params[:id]} with options: #{params}")
-                        event.respond(content: "✅ Drop successfully updated, thanks! #{params}", ephemeral: true)
-                    end
-                end
-                rescue => e
-                    event.respond(content: "💫 Whoops 💫 An unexpected error occurred. Send this to Ely: #{e.message} for drop ID: #{params[:id]} to have update #{params}", ephemeral: true)
-                    Rails.logger.error("Update failed: #{e.message} when updating drop ID #{params[:id]} with #{params}")
-                end
+                  UpdateDrop.call(event)
             end
+        end
+    end
+
+    def self.call(event)
+        params = {
+        id: event.options["id"],
+        item: event.options["drop_name"].present? ? Item.create(name: event.options["drop_name"]) : nil,
+        team: event.options["team"].present? ? Team.find_or_create_by(name: event.options["team"]) : nil,
+        owner: event.options["owner"],
+        submitter: event.options["submitter"],
+        reviewed_by: event.options["reviewed_by"],
+        status: event.options["status"].present? ? event.options["status"].downcase : nil,
+        }
+
+        # remove nil values!
+        params.compact!
+
+        # Get the member who called the command
+        member = event.user.on(event.server)
+
+        # Find the role named "Deputy Owners"
+        deputy_role = event.server.roles.find { |r| r.name == "Deputy Owners" }
+
+        unless deputy_role.present? && member&.role?(deputy_role)
+            Rails.logger.info("User #{event.server.member(event.user.id).display_name} tried to update drops but lacked the correct role.")
+            event.respond(content: "Sorry, you don't have permission to use this command.", ephemeral: true)
+            return
+        end
+
+        err = validate(params)
+        if err.present?
+            Rails.logger.info("User #{event.server.member(event.user.id).display_name} tried to update drops but an err occurred: #{err}.")
+            event.respond(content: err, ephemeral: true)
+            return
+        end
+
+        begin
+        Drop.transaction do
+            drop = Drop.find_by_id(params[:id])
+            if drop.update!(params)
+                Rails.logger.info("User #{event.server.member(event.user.id).display_name} updated drop ID #{params[:id]} with options: #{params}")
+                event.respond(content: "✅ Drop successfully updated, thanks! #{params}", ephemeral: true)
+            end
+        end
+        rescue => e
+            event.respond(content: "💫 Whoops 💫 An unexpected error occurred. Send this to Ely: #{e.message} for drop ID: #{params[:id]} to have update #{params}", ephemeral: true)
+            Rails.logger.error("Update failed: #{e.message} when updating drop ID #{params[:id]} with #{params}")
         end
     end
 
